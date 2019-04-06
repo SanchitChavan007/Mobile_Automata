c5460461ce4d88a59fbdf084d38bcacd97d9dddd
Sanchit Chavan <sanchitchavan@Sanchits-MacBook-Pro.local>
2019-04-06 03:26:57 -0400

    Added Rule C

--- src/edu/neu/csye6200/ma/MARuleC.java
+++ src/edu/neu/csye6200/ma/MARuleC.java
@@ -1,15 +1,115 @@
-//package edu.neu.csye6200.ma;
-//
-//public class MARuleC implements MARule {
-//
-//	public MARuleC()  {
-//		// TODO Auto-generated constructor stub
-//	}
-//
-//	@Override
-//	public MAFrame generateCellFrame(MAFrame ma, int i, int j) {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//}
+package edu.neu.csye6200.ma;
+
+import edu.neu.csye6200.ma.MACell.Color;
+
+public class MARuleC implements MARule {
+	int location = -1;
+	private final static String white = "WHITE";
+	private final static String green = "GREEN";
+	private final static String gray = "GRAY";
+	private final static String red = "RED";
+	private final static String blue = "BLUE";
+	MACell[][] a = null;
+
+
+	@Override
+	public MAFrame applyRule(MAFrame ma, int i) {
+		a = ma.getCellArray();
+		if(i>a.length) return null;
+		for(int j = 0; j<a[i].length; j++) {
+			if(a[i-1][j].getColor() == Color.GRAY) {
+				a[i][j].setColor(gray);
+			}
+		}
+		for(int j = 1; j<a[i].length; j++) {
+			if(a[i-1][j].getColor() == MACell.Color.BLUE) {
+					if(a[i-1][j-1].getColor()  == MACell.Color.GRAY && a[i-1][j+1].getColor()  == MACell.Color.GRAY) {
+						a[i][j].setColor(gray);
+						if(a[i][j-1].getColor() == MACell.Color.GRAY) {
+							a[i][j-1].setColor(blue);
+						}else {
+							a[i][j-1].setColor(red);
+						}
+					}else if(a[i-1][j-1].getColor()  == MACell.Color.GRAY && a[i-1][j+1].getColor()  == MACell.Color.WHITE) {
+						a[i][j].setColor(gray);
+						if(a[i][j+1].getColor() == MACell.Color.GRAY) {
+							a[i][j+1].setColor(blue);
+						}else {
+							a[i][j+1].setColor(red);
+						}
+					}else if(a[i-1][j+1].getColor()  == MACell.Color.GRAY && a[i-1][j-1].getColor()  == MACell.Color.WHITE) {
+						a[i][j].setColor(white);
+						if(a[i][j+1].getColor() == MACell.Color.GRAY) {
+							a[i][j+1].setColor(blue);
+						}else {
+							a[i][j+1].setColor(red);
+						}
+					}else if(a[i-1][j+1].getColor()  == MACell.Color.WHITE && a[i-1][j-1].getColor()  == MACell.Color.WHITE) {
+						a[i][j].setColor(gray);
+						if(a[i][j-1].getColor() == MACell.Color.GRAY) {
+							a[i][j-1].setColor(blue);
+						}else {
+							a[i][j-1].setColor(red);
+						}
+					}
+					
+			}else if(a[i-1][j].getColor() == MACell.Color.RED) {
+				if(a[i-1][j-1].getColor()  == MACell.Color.GRAY && a[i-1][j+1].getColor()  == MACell.Color.GRAY) {
+					a[i][j].setColor(gray);
+					if(a[i][j+1].getColor() == MACell.Color.GRAY) {
+						a[i][j+1].setColor(blue);
+					}else {
+						a[i][j+1].setColor(red);
+					}
+					
+				}else if(a[i-1][j-1].getColor()  == MACell.Color.GRAY && a[i-1][j+1].getColor()  == MACell.Color.WHITE) {
+					a[i][j].setColor(gray);
+					if(a[i][j-1].getColor() == MACell.Color.GRAY) {
+						a[i][j-1].setColor(blue);
+					}else {
+						a[i][j-1].setColor(red);
+					}
+					
+					
+				}else if(a[i-1][j+1].getColor()  == MACell.Color.GRAY && a[i-1][j-1].getColor()  == MACell.Color.WHITE) {
+					a[i][j].setColor(white);
+					if(a[i][j-1].getColor() == MACell.Color.GRAY) {
+						a[i][j-1].setColor(blue);
+					}else {
+						a[i][j-1].setColor(red);
+					}
+					
+					
+				}else if(a[i-1][j+1].getColor()  == MACell.Color.WHITE && a[i-1][j-1].getColor()  == MACell.Color.WHITE) {
+					a[i][j].setColor(gray);
+					if(a[i][j+1].getColor() == MACell.Color.GRAY) {
+						a[i][j+1].setColor(blue);
+					}else {
+						a[i][j+1].setColor(red);
+					}
+				}
+			}
+		}
+		return null;
+	}
+	
+	private void applyConditionB(int i, int j) {
+		a[i][j].setColor(gray);
+		if(a[i][j-1].getColor() == MACell.Color.GRAY) {
+			a[i][j-1].setColor(blue);
+		}else {
+			a[i][j-1].setColor(red);
+		}
+	}
+	
+	private void applyConditionA(int i, int j) {
+		a[i][j].setColor(white);
+		if(a[i][j+1].getColor() == MACell.Color.GRAY) {
+			a[i][j+1].setColor(blue);
+		}else {
+			a[i][j+1].setColor(red);
+		}
+	}
+
+
+}
